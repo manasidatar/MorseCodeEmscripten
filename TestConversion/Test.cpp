51e3e244c0bafc9c91bddd2d9ae518443f8724d3
Manasi Datar <manasi.datar@siemens-healthineers.com>
2017-08-11 16:07:11 +0200

    single word command line input works

--- TestConversion/Test.cpp
+++ TestConversion/Test.cpp
@@ -30,20 +30,30 @@
 
 int main(int argc, char** argv)
 {
+  // check if string is given
+  if (argc != 2)
+  {
+    std::cout <<
+      "Single word conversion: enter text word or Morse code with single-space separator" <<
+      std::endl;
+
+    return -1;
+  }
   // initialize
   MorseCodeConverter *testConverter = new MorseCodeConverter();
   std::string outStr("");
 
-  // perform conversion
-  testConverter->setTextString("sos");
-  testConverter->performConversion();
-  testConverter->getConvertedString(outStr);
-  std::cout << outStr << std::endl;
+  std::string inStr(argv[1]);
 
-  testConverter->setMorseString("... --- ...");
+  // perform conversion
+  if (inStr[0] == '.' || inStr[0] == '-')
+    testConverter->setMorseString(inStr);
+  else
+    testConverter->setTextString(inStr);
+
   testConverter->performConversion();
   testConverter->getConvertedString(outStr);
-  std::cout << outStr << std::endl;
+  std::cout << inStr << " --> " << outStr << std::endl;
 
   // clean up
   delete testConverter;
